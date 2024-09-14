51f526378907f24ad4bb953a110a182e222520bb
Cacai2 <motikifietkau@gmail.com>
2024-09-13 18:11:46 -0600

    More

--- PaginationLearningFietkau/obj/Debug/net8.0/PaginationLearningFietkau.AssemblyInfo.cs
+++ PaginationLearningFietkau/obj/Debug/net8.0/PaginationLearningFietkau.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaginationLearningFietkau")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60fda44f588e2e7b95062575046a4cca06783ae7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a276412e4583fea57e4924eb87d47cda16287eec")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaginationLearningFietkau")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaginationLearningFietkau")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
